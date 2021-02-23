a7ac9410d2e5822f3e7e0c6b6399e0b3980e701a
Sanghyunn <shawnpark98@naver.com>
2021-02-23 16:52:21 +0900

    Clock + Checkin

---
+++ clock.js
@@ -0,0 +1,28 @@
+const clockContainer = document.querySelector(".clock");
+const clockTitle = clockContainer.querySelector("h1");
+
+
+function loadDate(){
+    const date = new Date();
+
+    const Today = {
+        hours : date.getHours() ,
+        minutes : date.getMinutes() ,
+        seconds : date.getSeconds()
+    }
+
+    showDate(Today);
+}
+
+function showDate(Today){
+    clockTitle.innerText = `${
+        Today.hours < 10 ? `0${Today.hours}` : Today.hours }시 ${
+        Today.minutes < 10 ? `0${Today.minutes}` : Today.minutes }분 ${
+        Today.seconds < 10 ? `0${Today.seconds}` : Today.seconds }초 `
+}
+function Init(){
+    loadDate();
+	setInterval(loadDate, 1000);
+}
+
+Init();
---
+++ welcome.js
@@ -0,0 +1,69 @@
+const form = document.querySelector(".form");
+const input = form.querySelector("input");
+const welcome = document.querySelector(".welcome");
+const checkout = document.querySelector(".checkout");
+
+const USER_LS = "currentUser";
+const SHOW_ON = "displayShow";
+const SHOW_OFF = "displayNone";
+
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+
+    if(currentUser === null){
+        askForName();
+        
+    }
+
+    else {
+        showWelcome(currentUser);
+    }
+}
+
+function askForName(){
+    form.classList.remove(SHOW_OFF);
+    welcome.classList.add(SHOW_OFF);
+    checkout.classList.add(SHOW_OFF);
+
+    form.addEventListener("submit", handleSubmit);
+
+}
+    
+
+function showWelcome(user){  
+    welcome.innerText = `반가워요 ${user}님!`;
+    checkout.innerText = `혹시 ${user}님이 아니신가요?`;
+
+    form.classList.add(SHOW_OFF);
+    welcome.classList.remove(SHOW_OFF);
+    checkout.classList.remove(SHOW_OFF);
+
+    checkout.addEventListener("click", handleClick);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    
+    const currentValue = input.value;
+    localStorage.setItem(USER_LS, currentValue);
+
+    showWelcome(currentValue);
+}
+    
+function handleClick(event) {
+    event.preventDefault();
+    localStorage.removeItem(USER_LS);
+    
+    form.classList.remove(SHOW_OFF);
+    welcome.classList.add(SHOW_OFF);
+    checkout.classList.add(SHOW_OFF);
+
+    input.value = "";
+}
+
+
+function Init(){
+    loadName();
+}
+
+Init();
